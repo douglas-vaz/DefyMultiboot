0600cf0330fa2df84b6df8adae9546a3691b4712
Douglas Vaz <douglas1992@gmail.com>
2012-05-21 17:37:48 +0530

    Modified build script

--- build/buildzip.sh
+++ build/buildzip.sh
@@ -2,21 +2,21 @@
 
 # clone files into temp-dir
 mkdir -p ./tmp/
-cp -R ./META-INF ./tmp/
-cp -R ./system ./tmp/
+cp -R ../META-INF ./tmp/
+cp -R ../system ./tmp/
 
 # clean files
 find ./tmp/ -type f -name ".gitignore" -exec rm -f {} \;
 
 # create zip-archive
-rm -Rf ./out/*
-mkdir -p ./out/
+rm -Rf ../out/*
+mkdir -p ../out/
 cd  ./tmp
-zip -r ../out/multiboot-unsigned.zip *
-cd ..
+zip -r ../../out/multiboot-unsigned.zip *
+cd ../..
 
 # delete temp-folder
-rm -Rf ./tmp
+rm -Rf ./build/tmp
 
 # sign zip
 java -Xmx512m -jar ./build/signapk.jar -w ./build/testkey.x509.pem ./build/testkey.pk8 ./out/multiboot-unsigned.zip ./out/multiboot.zip
